2793b974cfc5af13f6f595a7fd685ab1ebb22199
rzegnam <rzegnam@gmail.com>
2019-11-14 17:39:50 +0100

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-1. yarn run install
-2. yarn run cypress
-3. execute signup.js in Cypress
+# Project
+
+Two test scenarios automated with Cypress:
+
+- TS2 Mandatory fields are required
+- TS3 Email validation
+
+from http://bit.ly/tslcases
+
+## Getting Started
+
+These step by step instructions will help to configure Cypress environment
+
+### Prerequisites
+
+yarn or npm
+
+### Installing
+
+Install Cypress via yarn:
+
+```
+yarn add cypress --dev
+```
+
+Install Cypress via npm:
+
+```
+npm install cypress --save-dev
+```
+
+## Running the tests
+
+1. Open Cypress with the script:
+
+```
+yarn run cypress
+```
+
+or with:
+
+```
+npm run cypress
+```
+
+2. When Cypress window is opened click on "signup.js" and watch the tests execution
--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -1,45 +1,49 @@
-describe('Checks the signup form', () => {
-  const { fakeEmail, fakePassword, badEmail, worstEmail, theWorstEmail } = Cypress.config()
+describe("Checks the signup form", () => {
+  const {
+    fakeEmail,
+    fakePassword,
+    badEmail,
+    worstEmail,
+    theWorstEmail
+  } = Cypress.config();
   beforeEach(() => {
-    cy.visit('/signup');
-  })
+    cy.visit("/signup");
+  });
 
-  it('should check for all the mandatory fields', () => {
-    cy.get('h1').contains("Let's get started")
-    cy.contains('Sign up for free').click()
-    cy.contains('Please enter a valid email address')
-    cy.contains('A password is required')
-    cy.get('input[name=email-address]')
-    .type(fakeEmail)
-    .should('have.value', fakeEmail)
-    cy.contains('A password is required')
-    cy.get('input[name=password]')
-    .type(fakePassword)
-    .should('have.value', fakePassword)
-    cy.contains('Sign up for free').click()
-    cy.contains('You must agree in order to use Toggl')
-    cy.url().should('include', '/signup')
-  })
+  it("TS2: should check for all the mandatory fields", () => {
+    cy.get("h1").contains("Let's get started");
+    cy.contains("Sign up for free").click();
+    cy.contains("Please enter a valid email address");
+    cy.contains("A password is required");
+    cy.get("input[name=email-address]")
+      .type(fakeEmail)
+      .should("have.value", fakeEmail);
+    cy.contains("A password is required");
+    cy.get("input[name=password]")
+      .type(fakePassword)
+      .should("have.value", fakePassword);
+    cy.contains("Sign up for free").click();
+    cy.contains("You must agree in order to use Toggl");
+    cy.url().should("include", "/signup");
+  });
 
-  it('validates email input', () => {
-    cy.get('input[name=email-address]')
-    .type(badEmail)
-    .should('have.value', badEmail)
-    cy.contains('Please enter a valid email address')
-    cy.get('input[name=email-address]').clear()
-  
+  it("TS3: validates email input", () => {
+    cy.get("input[name=email-address]")
+      .type(badEmail)
+      .should("have.value", badEmail);
+    cy.contains("Please enter a valid email address");
+    cy.get("input[name=email-address]").clear();
 
-    cy.get('input[name=email-address]')
-    .type(worstEmail)
-    .should('have.value', worstEmail)
-    cy.contains('Please enter a valid email address')
-    cy.get('input[name=email-address]').clear()
+    cy.get("input[name=email-address]")
+      .type(worstEmail)
+      .should("have.value", worstEmail);
+    cy.contains("Please enter a valid email address");
+    cy.get("input[name=email-address]").clear();
 
-    cy.get('input[name=email-address]')
-    .type(theWorstEmail)
-    .should('have.value', theWorstEmail)
-    cy.contains('Please enter a valid email address')
-    cy.get('input[name=email-address]').clear()
-
-  })
-})
+    cy.get("input[name=email-address]")
+      .type(theWorstEmail)
+      .should("have.value", theWorstEmail);
+    cy.contains("Please enter a valid email address");
+    cy.get("input[name=email-address]").clear();
+  });
+});
